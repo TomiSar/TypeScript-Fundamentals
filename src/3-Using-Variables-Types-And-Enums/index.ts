af0e3dccbb3a1f9c8bad43a733ff811b104b04d0
TomiSar <tomppa.sarjamo@gmail.com>
2026-04-22 22:03:38 +0300

    Some Changes to lessons

--- src/3-Using-Variables-Types-And-Enums/index.ts
+++ src/3-Using-Variables-Types-And-Enums/index.ts
@@ -1,17 +1,43 @@
 // string, number, boolean, array, undefined, null, any
 
-let firstName: string = 'Tomi';
-let lastName: string | null = 'Sarjamo';
-let age: number;
-age = 43;
+let firstName: string = 'Hannu';
+let lastName: string | null = 'Karpo';
+let age: number = 75;
 let hasPurchased = true;
 
 console.log(`Hello ${firstName} ${lastName} you are ${age} years old.`);
 
-let productName: string[] = ['Basketball', 'Skateboard', 'Pizza', 'Coca-Cola'];
+interface Product {
+  name: string;
+  price: number;
+}
 
+const products: Product[] = [
+  { name: 'Skateboard', price: 89.99 },
+  { name: 'Basketball', price: 29.99 },
+  { name: 'Pizza', price: 9.99 },
+  { name: 'Coca-Cola', price: 1.99 },
+  { name: 'Guitar', price: 199.99 },
+];
+
+console.log(`Available products at the store: ${products.length}`);
+for (let i = 0; i < products.length; i++) {
+  console.log(
+    `Product ${i + 1}: ${products[i].name}, Price: ${products[i].price}€`,
+  );
+}
+
+let productName: string[] = [
+  'Skateboard',
+  'Basketball',
+  'Pizza',
+  'Coca-Cola',
+  'Guitar',
+];
+
+console.log(`\nAvailable products at the store: ${productName.length}`);
 for (let i = 0; i < productName.length; i++) {
-  console.log(`${i}. ${productName[i]}`);
+  console.log(`Product name ${i + 1}: ${productName[i]}`);
 }
 
 let petCount: number[] = [];
